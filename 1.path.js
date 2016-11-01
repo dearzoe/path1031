185f5a6157600e49d81b91f19e54962f29ae917b
dearzoe <2421113739@qq.com>
2016-11-02 00:39:39 +0800

    1

--- 1.path.js
+++ 1.path.js
@@ -1,3 +1,10 @@
 var path=require("path");
 
-//no
+//normalize 将非标准的路径字符串转化为标准路径字符串
+/*
+* 1.可以
+*
+* */
+console.log(path.normalize('a/../b/j'));
+console.log(path.resolve());
+console.log(path.relative(__dirname,"/"))
